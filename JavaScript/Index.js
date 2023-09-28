5f08415df8aa05aae4a5fa9c4bf1fcc4c73627d3
Lucas Barrios <lucas.barrios@scania.com>
2023-09-28 13:49:17 -0300

    Atualização

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -1,42 +1,88 @@
 const vagas = document.querySelectorAll('.vaga');
 const tabelaReservas = document.getElementById('reservas');
 
-function randomizarStatusVagas() {
-    vagas.forEach((vaga) => {
-        const randomStatus = Math.random() < 0.5 ? 'livre' : 'ocupada';
-        vaga.classList.remove('livre', 'ocupada');
-        vaga.classList.add(randomStatus);
-    });
 
-    // Limpe a tabela de reservas ao randomizar as vagas
-    tabelaReservas.innerHTML = '';
+
+// Verifique se há reservas anteriores no localStorage e recupere-as
+const reservasAnteriores = JSON.parse(localStorage.getItem('reservas')) || [];
+
+// Função para adicionar uma reserva ao localStorage e à tabela
+function adicionarReserva(vagaIndex) {
+    const dataHora = new Date(); // Obtém a data e hora atuais
+    const dia = dataHora.toLocaleDateString();
+    const hora = dataHora.toLocaleTimeString();
+
+    // Crie um objeto com os detalhes da reserva
+    const reserva = {
+        vaga: `Vaga ${vagaIndex + 1}`,
+        data: dia,
+        hora: hora,
+    };
+
+    // Adicione a reserva ao array de reservas e ao localStorage
+    reservasAnteriores.push(reserva);
+    localStorage.setItem('reservas', JSON.stringify(reservasAnteriores));
+
+    // Crie uma nova linha na tabela de reservas
+    const newRow = tabelaReservas.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    cell1.innerHTML = reserva.vaga;
+    cell2.innerHTML = reserva.data;
+    cell3.innerHTML = reserva.hora;
 }
 
-// Chame a função para randomizar as vagas quando a página carregar
-window.addEventListener('load', randomizarStatusVagas);
-
+// Verifique se há vagas ocupadas previamente e atualize o estado das vagas
 vagas.forEach((vaga, index) => {
+    const reservaAnterior = reservasAnteriores.find(reserva => reserva.vaga === `Vaga ${index + 1}`);
+    
+    if (reservaAnterior) {
+        vaga.classList.remove('livre');
+        vaga.classList.add('ocupada');
+    }
+
     vaga.addEventListener('click', () => {
         if (vaga.classList.contains('livre')) {
             vaga.classList.remove('livre');
             vaga.classList.add('ocupada');
-            const dataHora = new Date();
-            const dia = dataHora.toLocaleDateString();
-            const hora = dataHora.toLocaleTimeString();
-
-            // Crie uma nova linha na tabela de reservas
-            const newRow = tabelaReservas.insertRow();
-            const cell1 = newRow.insertCell(0);
-            const cell2 = newRow.insertCell(1);
-            const cell3 = newRow.insertCell(2);
-            cell1.innerHTML = `Vaga ${index + 1}`;
-            cell2.innerHTML = dia;
-            cell3.innerHTML = hora;
-            document.getElementById('info').innerHTML = `Vaga ${index + 1} reservada em ${dia}`;
-            alert("A reserva foi definida.");
+            adicionarReserva(index);
         } else {
             document.getElementById('info').innerHTML = 'Esta vaga já está ocupada.';
-            alert("Vaga já ocupada.");
         }
     });
 });
+
+
+// Função para carregar as reservas do localStorage e exibi-las na tabela
+function carregarReservas() {
+    const reservasAnteriores = JSON.parse(localStorage.getItem('reservas')) || [];
+
+    reservasAnteriores.forEach((reserva) => {
+        const newRow = tabelaReservas.insertRow();
+        const cell1 = newRow.insertCell(0);
+        const cell2 = newRow.insertCell(1);
+        const cell3 = newRow.insertCell(2);
+        cell1.innerHTML = reserva.vaga;
+        cell2.innerHTML = reserva.data;
+        cell3.innerHTML = reserva.hora;
+    });
+}
+
+// Chame a função para carregar as reservas assim que a página for carregada
+window.addEventListener('load', carregarReservas);
+
+function resetarReservas() {
+    tabelaReservas.innerHTML = ''; // Limpa a tabela
+    localStorage.removeItem('reservas'); // Remove os dados do localStorage
+
+    // Torna todas as vagas livres
+    vagas.forEach((vaga) => {
+        vaga.classList.remove('ocupada');
+        vaga.classList.add('livre');
+    });
+}
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetarReservas);
+
